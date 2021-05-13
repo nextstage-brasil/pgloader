b1fa21715dca525c1e31718ff6311e2e2ef21c3d
Nextstage Brasil <ns@nextstage.com.br>
2021-05-13 09:58:55 -0300

    Versão 1.0

--- pgloader.php
+++ pgloader.php
@@ -29,33 +29,30 @@ $default = [
 // Documentação
 $help = ["Loader to Postgresql by Nextstage Brasil. Params: "];
 $help[] = "Usage: \n\tphp pgloader.php -h localhost -u postgres -w 123456 -p 5432 -d my_database -s public -x /dados/csv_to_import -t false -n tablename";
-//$help[] = "\tphp pgloader.php --help";
-//$help[] = "\tphp pgloader.php --version";
 $help[] = "Options: ";
-
 $options = [];
 foreach ($chaves as $key => $val) {
     $help[] = "\t-$key\t" . $val . ". Default: '" . $default[$key] . "'";
     $options[$key] = "$key:";
 }
 $help [] = '';
-//var_export($options);
+
+// Args
 $args = getopt(implode('', $options));
-//var_export($args);
-// pegar parametros mantendo o default
+
+// Args - set default
 $data = (object) array_replace($default, $args);
 $data->t = isset($data->t) ? true : false;
-//var_export($data);
-
 if (!is_dir($data->x)) {
     echo "\nERROR: Directory not found. " . $data->x . PHP_EOL;
     echo implode("\n", $help);
     die();
 }
 
+error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_startup_errors', 1);
+
 
 // conexão
 try {
